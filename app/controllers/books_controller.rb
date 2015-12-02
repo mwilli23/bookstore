fd0310b2db2766c879712fe0e241c89febe75008
Michael Williams <mwilli23@masonlive.gmu.eduemail>
2015-12-01 17:48:34 -0800

    added books index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
-class BooksController <ApplicationController
- def index
-  @available_at = Time.now
-  @books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
- end
+class BooksController < ApplicationController
+  def index
+  end
 end
