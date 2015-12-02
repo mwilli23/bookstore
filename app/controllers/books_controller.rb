94fe93787d0ee8cf3043b127712298783fbd315f
Michael Williams <mwilli23@masonlive.gmu.eduemail>
2015-12-01 19:50:40 -0800

    Finished Lab 7 in Class2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
+
   def index
    @available_at = Time.now
-   @books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+   @books = Book.all
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
 end
